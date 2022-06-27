c0b1d75c53825a25874d173b48c42ffae3bd9757
Nguyen Van Tu <tunv@rubyonring.com>
2022-06-27 14:28:03 +0700

    Update chapter 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,19 @@ User.create!(name: "Nguyen Tu",
              email: "abc@gmail.com",
              password: "111111",
              password_confirmation: "111111",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+)
 
-# 99.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n + 1}@railstutorial.org"
-#   password = "password"
-#   User.create!(name: name,
-#                email: email,
-#                password: password,
-#                password_confirmation: password)
-# end
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n + 1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
